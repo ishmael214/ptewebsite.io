da2f8bd70b082c6600dd4e7f05f04c9c0b37cca3
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-07-26 02:07:08 -0500

    javascript & UI changes

--- index.js
+++ index.js
@@ -3,45 +3,31 @@ const video = document.querySelector('.dvid');
 const togglePause = document.querySelector('.toggle-pause');
 const toggleSound = document.querySelector('.toggle-sound');
 
-
-function togglePlay() {
-  const isPlaying = video.paused ? 'play' : 'pause';
-  video[isPlaying]();
-}
-
-
-function togglePlyBtn() {
-  if (this.paused) {
-    togglePause.innerHTML = '<img src="play.png">';
-  } else {
+const pausePlay = () => {
+  if(video.paused) {
+    video.play();
     togglePause.innerHTML = '<img src="pause.png">';
+  } else {
+    video.pause();
+    togglePause.innerHTML = '<img src="play.png">';
   }
 }
 
-function toggleSnd () {
+togglePause.addEventListener('click', (e) => {
+  pausePlay();
+})
 
-}
+video.addEventListener('click', (e) => {
+  pausePlay();
+})
 
+toggleSound.addEventListener('click', (e) => {
+  video.muted = !video.muted;
 
-function toggleSndBtn() {
- if (this.muted === true) {
-     toggleSound.innerHTML = '<img  src="mute.png">' ;
- } else {
-     toggleSound.innerHTML = '<img src="sound.png">';
- }
-}
-
-
-
-video.addEventListener('click', toggleSnd);
-video.addEventListener('click', togglePlay);
-video.addEventListener('pause', togglePlyBtn);
-video.addEventListener('play', togglePlyBtn);
-video.addEventListener('muted', toggleSndBtn); 
-
-toggleSound.addEventListener('click', toggleSndBtn);
-
-togglePause.addEventListener('click', togglePlay);
-
-
+  if (video.muted) {
+    toggleSound.innerHTML = '<img  src="mute.png">';
+  } else {
+    toggleSound.innerHTML = '<img  src="sound.png">';
+  }
+})
 
